7dd0230af35ec349b92a3f69de2b2056130eaf8c
heeeete <mkoui@naver.com>
2024-12-04 21:12:11 +0900

    init

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import { Button, AlertButton } from '@keynut/ui';
+
+export default function Home() {
+  return (
+    <div>
+      <Button />
+      <AlertButton />
+    </div>
+  );
+}
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export const Button = () => {
+  return (
+    <button onClick={() => alert(`Haasdasdsdasdasdellasdasdo from your app!`)}>
+      butotototonasdjklasdjklasjdklasjdkl
+    </button>
+  );
+};
